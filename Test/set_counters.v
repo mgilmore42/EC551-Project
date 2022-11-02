59464a13117a2021d8641b49566dc48687807677
giacomocoraluppi <91134945+giacomocoraluppi@users.noreply.github.com>
2022-11-02 17:31:57 -0400

    adding set_counters test files

--- Test/set_counters.v
+++ Test/set_counters.v
@@ -1,1 +1,12 @@
-
+module set_counters(pixel_clk,raddr_vga,vga_hcnt,vga_vcnt);
+    
+    input pixel_clk;
+    input [18:0] raddr_vga;
+    output reg [10:0] vga_hcnt, vga_vcnt;
+    
+    always @ (posedge pixel_clk) begin
+    vga_hcnt = raddr_vga % 640;
+    vga_vcnt = raddr_vga / 640;
+    end
+ 
+endmodule
