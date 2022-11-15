e81954500de6a9840dfeed3a0a1c5daa634a5bf7
William Krska <wkrska@bu.edu>
2022-11-15 14:43:02 -0500

    file cleanup

--- Sources/IP/full_buffer/full_buffer_stub.v
+++ Sources/IP/full_buffer/full_buffer_stub.v
@@ -1,8 +1,8 @@
 // Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
-// Date        : Sun Nov  6 15:03:21 2022
-// Host        : bme-compsim-12 running 64-bit CentOS Linux release 7.9.2009 (Core)
+// Date        : Tue Nov 15 14:33:25 2022
+// Host        : eng-grid3 running 64-bit CentOS Linux release 7.9.2009 (Core)
 // Command     : write_verilog -force -mode synth_stub
 //               /ad/eng/users/w/k/wkrska/Documents/EC551/EC551_Project/Sources/IP/full_buffer/full_buffer_stub.v
 // Design      : full_buffer
