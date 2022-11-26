9d55897a3011010d868f611f71dba44b14c72615
Gilmore <mgilm0re@vlsi11.bu.edu>
2022-11-25 20:19:09 -0500

    snapshot

--- Sources/camera_interface.v
+++ Sources/camera_interface.v
@@ -1,59 +1,39 @@
 `timescale 1ns / 1ps
 
-module camera_interface(
+module camera_interface (
 		input wire clk,
-		input wire clk_100,
 		input wire rst_n,
 		//camera pinouts
-		input wire       cmos_pclk,
-		input wire       cmos_href,
-		input wire       cmos_vsync,
-		inout            cmos_sda,
-		inout            cmos_scl,
-		output wire      cmos_xclk
+		inout       cmos_sda,
+		inout       cmos_scl,
+		output reg  finished,
+		output wire cmos_xclk
 	);
-	
-	
-	//FSM state declarations
-	localparam idle          =  0;
-	localparam start_sccb    =  1;
-	localparam write_address =  2;
-	localparam write_data    =  3;
-	localparam digest_loop   =  4;
-	localparam delay         =  5;
-	localparam vsync_fedge   =  6;
-	localparam byte1         =  7;
-	localparam byte2         =  8;
-	localparam fifo_write    =  9;
-	localparam stopping      = 10;
 
-	localparam wait_init    = 0;
-	localparam sccb_idle    = 1;
-	localparam sccb_address = 2;
-	localparam sccb_data    = 3;
-	localparam sccb_stop    = 4;
+
+	//FSM state declarations
+	localparam idle          = 3'd0;
+	localparam start_sccb    = 3'd1;
+	localparam write_address = 3'd2;
+	localparam write_data    = 3'd3;
+	localparam digest_loop   = 3'd4;
+	localparam delay         = 3'd5;
+	localparam done          = 3'd6;
 
 	localparam MSG_INDEX = 3; //number of the last index to be digested by SCCB
 	 
 	reg        stop;
 	reg        start;
-	reg        wr_en;
-	reg        start_delay_q=0;
+	reg        start_delay_q;
 	reg        start_delay_d;
 	reg        delay_finish;
 
-	reg [ 2:0] sccb_state_q=0;
-	reg [ 2:0] sccb_state_d;
-	reg [ 3:0] state_q=0;
+	reg [ 3:0] state_q;
 	reg [ 3:0] state_d;
-	reg [ 7:0] addr_q;
-	reg [ 7:0] addr_d;
-	reg [ 7:0] data_q;
-	reg [ 7:0] data_d;
 	reg [ 7:0] wr_data;
-	reg [ 7:0] message_index_q=0;
+	reg [ 7:0] message_index_q;
 	reg [ 7:0] message_index_d;
-	reg [27:0] delay_q=0;
+	reg [27:0] delay_q;
 	reg [27:0] delay_d;
 
 	reg [15:0] message [3:0];
@@ -62,48 +42,46 @@ module camera_interface(
 	wire [1:0] ack;
 	wire [7:0] rd_data;
 	wire [3:0] state;
-	 
-	 //buffer for all inputs coming from the camera
-	 reg pclk_1,pclk_2,href_1,href_2,vsync_1,vsync_2;
 
-	 
+
 	initial begin //collection of all adddresses and values to be written in the camera {address,data}
-		message[0]=16'h12_80;  //reset all register to default values
-		message[1]=16'h12_04;  //set output format to RGB
-		message[2]=16'h15_20;  //pclk will not toggle during horizontal blank
-		message[3]=16'h8C_03;  //RGB444 in RG BX format
+		message[0] <= 16'h12_80;  //reset all register to default values
+		message[1] <= 16'h12_04;  //set output format to RGB
+		message[2] <= 16'h15_20;  //pclk will not toggle during horizontal blank
+		message[3] <= 16'h8C_03;  //RGB444 in RG BX format
+
+		// initializes stateful registers
+		state_q         <= idle;
+		delay_q         <= 0;
+		start_delay_q   <= 0;
+		message_index_q <= 0;
 	end
 
 	//register operations
-	always @(posedge clk,negedge rst_n) begin
+	always @(posedge clk, negedge rst_n) begin
 		if(!rst_n) begin
-			state_q         <= 0;
+			state_q         <= idle;
 			delay_q         <= 0;
 			start_delay_q   <= 0;
 			message_index_q <= 0;
-			
-			sccb_state_q <= 0;
-			addr_q       <= 0;
-			data_q       <= 0;
 		end else begin
 			state_q         <= state_d;
 			delay_q         <= delay_d;
 			start_delay_q   <= start_delay_d;
-			message_index_q <= message_index_d;			
-			pclk_1          <= cmos_pclk; 
-			pclk_2          <= pclk_1;
-			href_1          <= cmos_href;
-			href_2          <= href_1;
-			vsync_1         <= cmos_vsync;
-			vsync_2         <= vsync_1;
-
-			sccb_state_q <= sccb_state_d;
-			addr_q       <= addr_d;
-			data_q       <= data_d;
+			message_index_q <= message_index_d;
 		end
 	end
-	 	 
-	 
+
+	// determines if the camera initialization is complete
+	always @(*) begin
+		if (state == done) begin
+			finished = 1;
+		end else begin
+			finished = 0;
+		end
+	end
+
+
 	//FSM next-state logics
 	always @* begin
 		state_d         = state_q;
@@ -114,11 +92,6 @@ module camera_interface(
 		delay_d         = delay_q;
 		delay_finish    = 0;
 		message_index_d = message_index_q;
-		wr_en           = 0;
-
-		sccb_state_d = sccb_state_q;
-		addr_d       = addr_q;
-		data_d       = data_q;
 		
 		//delay logic  
 		if(start_delay_q) begin
@@ -134,9 +107,9 @@ module camera_interface(
 			start_delay_d = 0;
 			delay_d       = 0;
 		end
-		
+
 		case(state_q) 
-		
+
 			////////Begin: Setting register values of the camera via SCCB///////////
 			idle: begin
 				if(delay_finish) begin //idle for 0.6s to start-up the camera
@@ -148,45 +121,54 @@ module camera_interface(
 			end
 
 			start_sccb: begin   //start of SCCB transmission
-				start=1;
-				wr_data=8'h42; //slave address of OV7670 for write
-				state_d=write_address;						
+				start   = 1;
+				wr_data = 8'h42; //slave address of OV7670 for write
+				state_d = write_address;
 			end
 
 			write_address: begin
-				if(ack==2'b11) begin 
-					wr_data=message[message_index_q][15:8]; //write address
-					state_d=write_data;
+				if (ack == 2'b11) begin 
+					wr_data = message[message_index_q][15:8]; //write address
+					state_d = write_data;
 				end
 			end
 
 			write_data: begin
-				if(ack==2'b11) begin 
-					wr_data=message[message_index_q][7:0]; //write data
-					state_d=digest_loop;
+				if (ack == 2'b11) begin 
+					wr_data = message[message_index_q][7:0]; //write data
+					state_d = digest_loop;
 				end
 			end
 
 			digest_loop: begin
-				if(ack==2'b11) begin //stop sccb transmission
-					stop=1;
-					start_delay_d=1;
-					message_index_d=message_index_q+1'b1;
-					state_d=delay;
+				if (ack == 2'b11) begin //stop sccb transmission
+					stop            = 1;
+					start_delay_d   = 1;
+					message_index_d = message_index_q + 1'b1;
+					state_d         = delay;
 				end
 			end
 
 			delay: begin
-				if(message_index_q==(MSG_INDEX+1) && delay_finish) begin 
-					state_d=vsync_fedge; //if all messages are already digested, proceed to retrieving camera pixel data
-				end else if(state==0 && delay_finish) begin
-					state_d=start_sccb; //small delay before next SCCB transmission(if all messages are not yet digested)
+				if ((message_index_q == MSG_INDEX + 1) && delay_finish) begin 
+					state_d = done; //if all messages are already digested, proceed to retrieving camera pixel data
+				end else if ((state == 0) && delay_finish) begin
+					state_d = start_sccb; //small delay before next SCCB transmission(if all messages are not yet digested)
 				end
 			end
+
+			done: begin
+				state_d = done;
+			end
+
+			default: begin
+				state_d = state_q;
+			end
+
 		endcase
 	end
-	 
-	 //module instantiations
+
+	//module instantiations
 	i2c_top #(.freq(100_000)) m0 (
 		.clk     ( clk      ),
 		.rst_n   ( rst_n    ),
@@ -200,16 +182,16 @@ module camera_interface(
 		.sda     ( cmos_sda ),
 		.state   ( state    )
 	); 
-	 
-	 
+
+	// TODO: need to verify clock frequency since its technology dependant
 	dcm_24MHz m1 (
 		// Clock in ports
-		.clk ( clk ),      // IN
+		.clk       ( clk       ),     // IN
 		// Clock out ports
 		.cmos_xclk ( cmos_xclk ),     // OUT
 		// Status and control signals
-		.RESET  ( ~rst_n ),// IN
-		.LOCKED (        )
+		.RESET     ( ~rst_n    ),// IN
+		.LOCKED    (           )
 	);      // OUT
-	
+
 endmodule
--- Sources/top_test.v
+++ Sources/top_test.v
@@ -1,78 +1,97 @@
 `timescale 1ns / 1ps
 
+`include "my_header.vh"
+
 module top_test (
-		input wire clk,rst_n,
-		input wire[3:0] key, //key[1:0] for brightness control , key[3:2] for contrast control
-		//camera pinouts
-		input wire cmos_pclk,cmos_href,cmos_vsync,
-		input wire[7:0] cmos_db,
-		inout cmos_sda,cmos_scl, 
-		output wire cmos_rst_n, cmos_pwdn, cmos_xclk,
-		//VGA output
-		output wire[3:0] vga_out_r,
-		output wire[3:0] vga_out_g,
-		output wire[3:0] vga_out_b,
-		output wire vga_out_vs,vga_out_hs
-	);
-	 
-	wire        f2s_data_valid;
-	wire [ 9:0] data_count_r;
-	wire [15:0] dout;
-	wire [15:0] din;
-	wire        clk_sdram;
-	wire        empty_fifo;
-	wire        clk_vga;
-	wire        state;
-	wire        rd_en;
 
-	//control logic for retrieving data from camera, storing data to asyn_fifo, and  sending data to sdram
-	camera_interface m0 (
-		.clk     (clk       ),
-		.clk_100 (clk_sdram ),
-		.rst_n   (rst_n     ),
-		.key     (key       ),
-		//asyn_fifo IO
-		.rd_en        ( f2s_data_valid ),
-		.data_count_r ( data_count_r   ),
-		.dout         ( dout           ),
-		//camera pinouts
-		.cmos_pclk  ( cmos_pclk  ),
-		.cmos_href  ( cmos_href  ),
-		.cmos_vsync ( cmos_vsync ),
-		.cmos_db    ( cmos_db    ),
-		.cmos_sda   ( cmos_sda   ),
-		.cmos_scl   ( cmos_scl   ), 
-		.cmos_rst_n ( cmos_rst_n ),
-		.cmos_pwdn  ( cmos_pwdn  ),
-		.cmos_xclk  ( cmos_xclk  )
+	    // General
+	    input wire CLK100MHZ,
+	    input wire rst_n,
+	    input wire [15:0] SW,
+	    
+	    // Camera I/O
+
+	    input wire [7:0] wdata_cam,
+	    input wire vsync_cam,
+	    input wire href_cam,
+	    input wire pclk_cam,
+	    output wire mclk_cam,
+	    inout i2c_sda,
+	    inout i2c_scl,
+
+	    // input wire [3:0] BTNS,
+	    
+	    // VGA I/O
+	    output wire [3:0] VGA_R,
+	    output wire [3:0] VGA_G,
+	    output wire [3:0] VGA_B,
+	    output wire VGA_HS,
+	    output wire VGA_VS
     );
-	 
-	//control logic for retrieving data from sdram, storing data to asyn_fifo, and sending data to vga
-	vga_interface m2 (
-		.clk   ( clk   ),
-		.rst_n ( rst_n ),
-		//asyn_fifo IO
-		.empty_fifo ( empty_fifo ),
-		.din        ( din        ),
-		.clk_vga    ( clk_vga    ),
-		.rd_en      ( rd_en      ),
-		//VGA output
-		.vga_out_r  ( vga_out_r  ),
-		.vga_out_g  ( vga_out_g  ),
-		.vga_out_b  ( vga_out_b  ),
-		.vga_out_vs ( vga_out_vs ),
-		.vga_out_hs ( vga_out_hs )
+    
+    // VGA Memory access
+    wire [`awidth_fbuff-1:0]            raddr_vga;
+    wire [`dwidth_dat-1:0]              rdata_vga;
+    
+    // Processing access
+    wire [`awidth_pbuff-1:0]             raddr_alu;
+    reg  [`awidth_fbuff-1:0]             waddr_alu;
+    wire [`dwidth_dat-1:0]               wdata_alu;
+    reg                                  wen_alu;
+    wire [(`dwss*`dwidth_dat)-1:0]       rdata_alu;
+    wire [(`dwss*`dwidth_kernel)-1:0]    kernel_alu;
+    wire [`dwidth_div-1:0]               div_alu;
+    
+    mem_controller mc(
+        .sys_clk   ( CLK100MHZ ),
+        .rst       ( ~rst_n    ), 
+        .pclk      ( pclk_cam  ),
+        .vsync_cam ( vsync_cam ),
+        .href_cam  ( href_cam  ),
+        .wdata_cam ( wdata_cam ),
+        .pass_thru ( SW[0]     ),
+        .raddr_vga ( raddr_vga ),
+        .rdata_vga ( rdata_vga ),
+        .waddr_alu ( waddr_alu ),
+        .wdata_alu ( wdata_alu ),
+        .wen_alu   ( wen_alu   ),
+        .raddr_alu ( raddr_alu ),
+        .rdata_alu ( rdata_alu )
     );
-	
-	//SDRAM clock
-	dcm_165MHz m3 (// Clock in ports
-		.clk( clk ),      // IN
-		// Clock out ports
-		.clk_sdram( clk_sdram ),     // OUT
-		// Status and control signals
-		.RESET  ( RESET  ),// IN
-		.LOCKED ( LOCKED )
-	);      // OUT
 
+    // ALU alu(
+    //     .din(rdata_alu),
+    //     .kernel(kernel_alu),
+    //     .div(div_alu),
+    //     .dout(wdata_alu)
+    // );
 
+    // kernel_ROM kr(
+    //     .kernel_select(SW[2:1]),
+    //     .kernel(kernel_alu),
+    //     .div(div_alu)
+    // );
+    
+    vga vga0(
+        .CLK100MHZ(CLK100MHZ),
+        .rst       ( ~rst_n    ),
+        .rdata_vga ( rdata_vga ),
+        .VGA_R     ( VGA_R     ),
+        .VGA_G     ( VGA_G     ),
+        .VGA_B     ( VGA_B     ),
+        .VGA_HS    ( VGA_HS    ),
+        .VGA_VS    ( VGA_VS    ),
+        .raddr_vga ( raddr_vga )
+    );
+    
+    camera_interface ca(
+        .clk_100(CLK100MHZ),
+        .rst_n(rst_n),
+        // .key(BTNS), // not including for now
+        
+        //camera pinouts
+        .cmos_sda(i2c_sda),         //inout!!
+        .cmos_scl(i2c_scl),         //i2c comm wires
+        .cmos_xclk(mclk_cam)
+    );
 endmodule
